cca07d07c1bb1580ed381afb6918a1e369ceaf19
Paul <colandreapaul@gmail.com>
2023-06-23 10:26:37 -0400

    init

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full backdrop-blur-md backdrop-saturate-150">
+      <nav className="mx-auto w-full px-3 py-3">
+        <div></div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/context/theme-provider.tsx
@@ -0,0 +1,29 @@
+"use client";
+import * as React from "react";
+
+import { ThemeProvider as TProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+
+type ProviderProps = {
+  children: React.ReactNode;
+} & ThemeProviderProps;
+
+const AppContext = React.createContext<{ fontsLoaded: boolean }>({
+  fontsLoaded: false,
+});
+
+export const useAppContext = () => React.useContext(AppContext);
+
+export function ThemeProvider({ children, ...props }: ProviderProps) {
+  return (
+    <TProvider
+      {...props}
+      defaultTheme={"light"}
+      attribute="class"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <>{children}</>
+    </TProvider>
+  );
+}
